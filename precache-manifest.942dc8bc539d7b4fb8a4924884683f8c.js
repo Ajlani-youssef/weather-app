a79e5dbca648856c9da754e78cbd5be2fbd5b8f0
Ajlani-youssef <youssefajlani@insat.u-carthage.tn>
2020-09-28 00:52:36 +0200

    Updates

---
+++ precache-manifest.942dc8bc539d7b4fb8a4924884683f8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1dfb2869b962c53d96553dba7e52dc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "9b818934e08e5159d9d5",
+    "url": "/weather-app/static/css/main.471f0a08.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b818934e08e5159d9d5",
+    "url": "/weather-app/static/js/main.d80e95b2.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "929dc834e5ead073fd8f13875f2e4a21",
+    "url": "/weather-app/static/media/cold.929dc834.jpg"
+  },
+  {
+    "revision": "0a64b4ea415643577ca7f5cfa9a3e194",
+    "url": "/weather-app/static/media/warm.0a64b4ea.jpg"
+  }
+]);
